6d280d849a44d7382550102d6c2ddb83675fede9
nobodj <dongxuan09@naver.com>
2018-06-09 18:06:54 +0900

    try~catch절추가

--- template.callback/src/main/java/space/nobodj/template/callback/Calculator.java
+++ template.callback/src/main/java/space/nobodj/template/callback/Calculator.java
@@ -4,19 +4,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
 public class Calculator {
+	Logger logger = Logger.getLogger(getClass());
 	
-	public int calcSum(String filepath) throws NumberFormatException, IOException{
+	public int calcSum(String filepath) throws IOException{
+		BufferedReader br = null;
 		
-		BufferedReader br = new BufferedReader(new FileReader(filepath));
-		int sum = 0;
-		String line = null;
-		while((line=br.readLine())!=null){
-			sum += Integer.valueOf(line);
+		try{
+			br = new BufferedReader(new FileReader(filepath));
+			int sum = 0;
+			String line = null;
+			while((line=br.readLine())!=null){
+				sum += Integer.valueOf(line);
+			}
+				
+			return sum;
+		} catch(IOException e){
+			logger.debug(e.getMessage());
+			throw e;
+		} finally {
+			if(br!=null){
+				try{
+					br.close();
+				} catch(IOException e){
+					logger.debug(e.getMessage());
+				}
+			}
 		}
 		
-		br.close();
-		
-		return sum;
 	}
 }
--- template.callback/src/test/java/space/nobodj/template/callback/CalculatorTest.java
+++ template.callback/src/test/java/space/nobodj/template/callback/CalculatorTest.java
@@ -5,14 +5,17 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class CalculatorTest {
+	Logger logger = Logger.getLogger(getClass());
 	
 	@Test
 	public void calcSumTest() throws NumberFormatException, IOException{
 		Calculator cal = new Calculator();
 		int sum = cal.calcSum(getClass().getResource("/numbers.txt").getPath());
+		logger.debug("sum="+sum);
 		assertThat(sum, is(10));
 	}
 	
